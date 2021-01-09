d232d60867ef45c804ee06add65352ff28a6dbe7
Peterson Medina <opetersonmedina@outlook.com>
2021-01-09 13:23:43 -0300

    Resolucao Database Upload

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,25 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import { validate } from 'uuid';
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const validateID = validate(id);
+
+    if (!validateID) {
+      throw new AppError('Transaction does not exist.');
+    }
+
+    const transaction = await transactionsRepository.findOne({ where: { id } });
+
+    if (!transaction) {
+      throw new AppError('Transaction does not exist!');
+    }
+
+    await transactionsRepository.remove(transaction);
   }
 }
 
